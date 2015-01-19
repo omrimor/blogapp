60a738ecec94a23a552e85fab5bbed209aff0f74
Omri Mor <omrim@perion.com>
2015-01-19 14:31:02 +0200

    start structuring the app

--- app/app.js
+++ app/app.js
@@ -1,3 +1,24 @@
 (function () {
-	// You app starts here
+	'use strict';
+	var app = angular.module('Blogapp', ['ngRoute']);
+
+	app.config(['$routeProvider', function($routeProvider){
+		$routeProvider
+			.when('/',{
+				templateUrl: 'partials/main.html',
+				controller: 'MainCtrl'
+			})
+			.when('/login',{
+				templateUrl: 'partials/login.html',
+				controller: 'LoginCtrl'
+			})
+			.when('/employee/:employeeId',{
+				templateUrl: 'partials/employee.html',
+				controller: 'employeeCtrl'
+			})
+
+			.otherwise({
+				redirectTo: '/'
+			});
+	}]);
 }());
